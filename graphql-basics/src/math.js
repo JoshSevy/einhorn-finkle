518381a695d43e8f99d0f7c834bfaacb27bff843
JoshSevy <joshuasevy@outlook.com>
2021-05-24 18:22:09 -0600

    Add directions for challenge one

---
+++ graphql-basics/src/math.js
@@ -0,0 +1,6 @@
+//TODO Define add function that takes two arguments and adds them up
+//TODO Define subtract function that takes two arguments and subtracts them
+//TODO Setup add as a default export
+//TODO Setup subtract as a named export
+//TODO Import both functions into index.js
+//TODO Use both functions and print the result from each
