8428b3abed908bc6183e9f2eece9f1b7b84959df
JoshSevy <joshuasevy@outlook.com>
2021-06-05 14:21:03 -0600

    Bootstrap all import to index

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,12 +1,23 @@
 //THIRD PARTY IMPORTS
 import { GraphQLServer } from 'graphql-yoga';
-import uuidv4 from 'uuid/v4';
+
 //CUSTOM IMPORTS
 import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers,
+  resolvers: {
+    Query,
+    Mutation,
+    User,
+    Post,
+    Comment
+  },
   context: {
     db
   }
