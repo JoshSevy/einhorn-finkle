c8f463bf2546a4a070ec5aaf430935513b90eec3
JoshSevy <joshuasevy@outlook.com>
2021-06-05 13:03:00 -0600

    Cleaning up file structure

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,49 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga';
 import uuidv4 from 'uuid/v4';
 
-//! 5 Scalar GraphQL Types: String, Boolean, Int, Float, ID
-//! Add Comment Type and resolvers
-//!1. Set up a "Comment" type with id and text fields. Non-nullable
-//!2. Set up a "comments" array with a comments
-//!3. Set up a "comments" query with a resolver that returns all the comments
-//!4. Run a query to get all a comments with both id and text fields.
-
-//! Connect Comment and User
-//! GOALS: Set up a relationship between Comment and User
-//!1. Set up an author field on Comment
-//!2. Update all comments in the array to have a new author field that returns the user who wrote the comment
-//!3. Create a resolver for the Comments author field that returns the user who wrote the comment
-//!4. Run a sample query that gets all comments and gets the authors name
-//!5. Set up a comment field on User
-//!6. Set up a resolver for the User comments field that returns all comments belonging to that user
-//!7. Run a sample query that gets all users and all those comments
-
-//! Connect Post and Comment
-//! GOALS: Set up relationship between Post and Comment
-
-//!1. Set up a post field on Comment
-//!2. Update all the comments in the array to have a new post field *use one of the post ids as value
-//!3. Create a resolver for the Comments post field that returns the post that the comment belongs to
-//!4. Run a sample query that gets all comments and gets the post name
-//!5. Set up a comments field on Post
-//!6. Set up a resolver for the Post comments field that returns all comments belonging to that post
-//!7. Run a sample query that gets all posts and all their comments
-
-//TODO Comment Mutation
-
-//TODO Goal: Allow clients to create a new comment
-
-//!1. Define a new createComment mutation
-//! - Should take text, author, post
-//! - Should return a comment
-//2. Define a resolver method for createComment
-// - Confirm that the user exists and is published, else throw error
-// - If they do exist, create the comment and return it
-//3. Run the mutation and add a comment
-//4. Use the commets query to verify the comment was added
-
-//TODO Remove a post and relational data with a mutation
-
 //* Dummy user data
 let users = [{
   id: '1',
@@ -101,69 +58,7 @@ let comments = [{
   post: '13'
 }];
 
-//* TYPE DEFINITIONS(schema)
-const typeDefs = `
-  type Query {
-    users(query: String): [User!]!
-    posts(query: String): [Post!]!
-    post: Post!
-    me: User!
-    comments: [Comment!]
-  }
 
-  type Mutation {
-    createUser(data: CreateUserInput!): User!
-    deleteUser(id: ID!): User!
-    createPost(data: CreatePostInput!): Post!
-    deletePost(id: ID!): Post!
-    createComment(data: CreateCommentInput!): Comment!
-    deleteComment(id: ID!): Comment!
-  }
-
-  input CreateUserInput {
-    name: String!
-    email: String!
-    age: Int
-  }
-
-  input CreatePostInput {
-    title: String!
-    body: String!
-    published: Boolean!,
-    author: ID!
-  }
-
-  input CreateCommentInput {
-    text: String!
-    author: ID!
-    post: ID!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]
-    comments: [Comment!]
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    author: User!
-    post: Post!
-  }
-`
 
 //* RESOLVERS
 const resolvers = {
