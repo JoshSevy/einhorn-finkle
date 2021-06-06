9e8040041d2b8b92ddb9c5ef4b69fa13657982b9
JoshSevy <joshuasevy@outlook.com>
2021-06-06 08:50:45 -0600

    Add comment subscription

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -12,6 +12,17 @@ const Subscription = {
 
       return pubSub.asyncIterator('count');
     }
+  },
+  comment: {
+    subscribe(parent, { postId }, { db, pubSub }, info) {
+      const post = db.posts.find(post => post.id === postId && post.published);
+
+      if (!post) {
+        throw new Error("Post not found");
+      }
+
+      return pubSub.asyncIterator(`comment ${postId}`)
+    }
   }
 };
 
