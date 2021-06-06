a6d50a680303674880180ab4f668c42f9e78f5f5
JoshSevy <joshuasevy@outlook.com>
2021-06-05 19:33:05 -0600

    Begin setting up a subscription for graph

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,25 +1,30 @@
 //THIRD PARTY IMPORTS
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 //CUSTOM IMPORTS
 import db from './db';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
+
+const pubSub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
   },
   context: {
-    db
+    db,
+    pubSub
   }
 });
 
