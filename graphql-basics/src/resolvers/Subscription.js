24ae91c80e9f537321e09d0c9eb8457edcedec20
JoshSevy <joshuasevy@outlook.com>
2021-06-09 18:07:48 -0600

    Add post sub type

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -8,20 +8,6 @@
 
 
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubSub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++
-        pubSub.publish('count', {
-          count
-        })
-      }, 1000)
-
-      return pubSub.asyncIterator('count');
-    }
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubSub }, info) {
       const post = db.posts.find(post => post.id === postId && post.published);
